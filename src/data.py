df385c6ae4c331e5aab6ca880d5b5e217b2774fd
CharlesShen <ssa19990208@foxmail.com>
2022-05-08 22:07:00 +0800

    fix bug

--- src/data.py
+++ src/data.py
@@ -2,13 +2,14 @@ import os
 import re
 import xml.dom.minidom
 import json
+import glob
 import numpy as np
 
 def read_tsp_data(file_name):
     """Read one tsp file
 
     Args:
-        file_name [str]: [the file name of the file] 
+        file_name [str]: [the full_file name of the file] 
         
     Returns:
         N [int]: [the number of nodes]
@@ -17,14 +18,14 @@ def read_tsp_data(file_name):
     """
     #base info
     base_path = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.pardir, 'data'))
-    full_name = os.path.join(base_path, file_name)
+    base_name = file_name.split(os.sep)[-1][:-4]
     best_name = os.path.join(base_path, 'optimal.json')
     pattern = re.compile(r'\d+')
-    N = int(pattern.findall(file_name[:-4])[0])
+    N = int(pattern.findall(base_name)[0])
     graph = np.zeros((N, N), dtype=np.float64)
     
     #read xml
-    root = xml.dom.minidom.parse(full_name).documentElement.getElementsByTagName('graph')[0]
+    root = xml.dom.minidom.parse(file_name).documentElement.getElementsByTagName('graph')[0]
     vertexs = root.getElementsByTagName('vertex')
     for start in range(N):
         vertex = vertexs[start]
@@ -37,7 +38,7 @@ def read_tsp_data(file_name):
     #read optimal
     f = open(best_name, 'r', encoding='utf8')
     json_data = json.load(f)
-    best_result = json_data[file_name[:-4]]
+    best_result = json_data[base_name]
     f.close()
     
     return N, graph, best_result
@@ -49,13 +50,13 @@ def get_data_names():
         file_names [list of str]: [the file names of the problems]
     """
     base_path = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.pardir, 'data'))
-    file_names = os.listdir(base_path)
+    file_names = glob.glob(os.path.join(base_path, '*.xml'))
     return file_names
 
 file_names = get_data_names()
 for file_name in file_names:
     N, graph, best_result = read_tsp_data(file_name)
-    print(file_name[:-4])
+    print(file_name.split(os.sep)[-1][:-4])
     print(N)
     print(best_result)
     print(graph)
